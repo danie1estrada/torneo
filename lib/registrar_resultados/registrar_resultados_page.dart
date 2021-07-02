5f6c28e1f345d591ff387ddafb54538838ec5c7f
Daniel Estrada <daniel.estrada@bowheadhealth.com>
2021-07-01 20:03:01 -0500

    fix scroll

--- lib/registrar_resultados/registrar_resultados_page.dart
+++ lib/registrar_resultados/registrar_resultados_page.dart
@@ -11,7 +11,8 @@ class RegistrarResultadosPage extends StatefulWidget {
 class _RegistrarResultadosPageState extends State<RegistrarResultadosPage> {
 
   List<Equipo> equipos = [];
-  String equipo1, equipo2;
+  String equipo1 = '';
+  String equipo2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -22,76 +23,78 @@ class _RegistrarResultadosPageState extends State<RegistrarResultadosPage> {
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Form(
-          child: Column(
-            children: [
-              DropdownButtonFormField<String>(
-                isExpanded: true,
-                value: equipo1,
-                onChanged: (value) {
-                  setState(() {
-                    equipo1 = value;                    
-                  });
-                },
-                items: equipos.map<DropdownMenuItem<String>>((equipo) => DropdownMenuItem<String>(
-                  value: equipo.nombre,
-                  child: Text(equipo.nombre)
-                )).toList(),
-                decoration: InputDecoration(
-                  labelText: "Equipo 1"
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                DropdownButtonFormField<String>(
+                  isExpanded: true,
+                  value: equipo1,
+                  onChanged: (value) {
+                    setState(() {
+                      equipo1 = value;                    
+                    });
+                  },
+                  items: equipos.map<DropdownMenuItem<String>>((equipo) => DropdownMenuItem<String>(
+                    value: equipo.nombre,
+                    child: Text(equipo.nombre)
+                  )).toList(),
+                  decoration: InputDecoration(
+                    labelText: "Equipo 1"
+                  ),
                 ),
-              ),
-              
-              SizedBox(height: 24,),
+                
+                SizedBox(height: 24,),
 
-              DropdownButtonFormField<String>(
-                isExpanded: true,
-                value: equipo2,
-                onChanged: (value) {
-                  setState(() {
-                    equipo2 = value;                    
-                  });
-                },
-                items: equipos.map<DropdownMenuItem<String>>((equipo) => DropdownMenuItem<String>(
-                  value: equipo.nombre,
-                  child: Text(equipo.nombre)
-                )).toList(),
-                decoration: InputDecoration(
-                  labelText: "Equipo 2"
+                DropdownButtonFormField<String>(
+                  isExpanded: true,
+                  value: equipo2,
+                  onChanged: (value) {
+                    setState(() {
+                      equipo2 = value;                    
+                    });
+                  },
+                  items: equipos.map<DropdownMenuItem<String>>((equipo) => DropdownMenuItem<String>(
+                    value: equipo.nombre,
+                    child: Text(equipo.nombre)
+                  )).toList(),
+                  decoration: InputDecoration(
+                    labelText: "Equipo 2"
+                  ),
                 ),
-              ),
     
-              SizedBox(height: 24,),
+                SizedBox(height: 24,),
 
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                    
-                      decoration: InputDecoration(
-                        labelText: "Puntos"
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                      
+                        decoration: InputDecoration(
+                          labelText: "Puntos"
+                        ),
                       ),
                     ),
-                  ),
 
-                  SizedBox(width: 24,),
+                    SizedBox(width: 24,),
 
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Puntos"
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Puntos"
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
 
-              SizedBox(height: 24,),
+                SizedBox(height: 24,),
 
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Confirmar")
-              )
-            ]
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Confirmar")
+                )
+              ]
+            ),
           ),
         ),
       ),
